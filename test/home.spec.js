16823aed0fb59e2bd0504c86347fdc3470710866
jasminhusadzic <jasmin.h@aleacontrol.com>
2020-04-21 18:36:07 +0200

    Run all home tests

--- test/home.spec.js
+++ test/home.spec.js
@@ -25,91 +25,91 @@ describe("home page suite", ()=>{
           });
     })
 
-    // it("search rice after we search for chicken and check load time when user is not logged in", ()=>{
-    //     HomePage.search('chicken');
-    //     let duration = HomePage.getSearchDuration('rice');
-    //     expect(duration).toBeLessThanOrEqual(60);
-    //     TimelineReporter.addContext({
-    //         title: 'Search duration is',
-    //         value: duration.toString() + " seconds"
-    //       });
-    // })
-    // it("search chicken, search rice and search chicken again and check load time for last search when user is not logged in", ()=>{
-    //     HomePage.search('chicken');
-    //     HomePage.search('rice');
-    //     let duration = HomePage.getSearchDuration('chicken');
-    //     expect(duration).toBeLessThanOrEqual(60);
-    //     TimelineReporter.addContext({
-    //         title: 'Search duration is',
-    //         value: duration.toString() + " seconds"
-    //       });
-    // })
+    it("search rice after we search for chicken and check load time when user is not logged in", ()=>{
+        HomePage.search('chicken');
+        let duration = HomePage.getSearchDuration('rice');
+        expect(duration).toBeLessThanOrEqual(60);
+        TimelineReporter.addContext({
+            title: 'Search duration is',
+            value: duration.toString() + " seconds"
+          });
+    })
+    it("search chicken, search rice and search chicken again and check load time for last search when user is not logged in", ()=>{
+        HomePage.search('chicken');
+        HomePage.search('rice');
+        let duration = HomePage.getSearchDuration('chicken');
+        expect(duration).toBeLessThanOrEqual(60);
+        TimelineReporter.addContext({
+            title: 'Search duration is',
+            value: duration.toString() + " seconds"
+          });
+    })
 
-    // it("search beef and check load time when user is not logged in", ()=>{
-    //     let duration = HomePage.getSearchDuration('beef');
-    //     expect(duration).toBeLessThanOrEqual(60);
-    //     if(duration>=10){
-    //         TimelineReporter.addContext('WARNING');
-    //     }
-    //     TimelineReporter.addContext({
-    //         title: 'Search duration is',
-    //         value: duration.toString() + " seconds"
-    //       });
-    // })
+    it("search beef and check load time when user is not logged in", ()=>{
+        let duration = HomePage.getSearchDuration('beef');
+        expect(duration).toBeLessThanOrEqual(60);
+        if(duration>=10){
+            TimelineReporter.addContext('WARNING');
+        }
+        TimelineReporter.addContext({
+            title: 'Search duration is',
+            value: duration.toString() + " seconds"
+          });
+    })
 
-    // it("search chicken and check load time when user is logged in", ()=>{
-    //     LoginPage.open();
-    //     LoginPage.login('jasmin.husadzic@gmail.com', 'test123');
-    //     LoginPage.waitForCart();
-    //     expect(HomePage.cart).toBeDisplayed;
-    //     let duration = HomePage.getSearchDuration('chicken');
-    //     expect(duration).toBeLessThanOrEqual(60);
-    //     TimelineReporter.addContext({
-    //         title: 'Search duration is',
-    //         value: duration.toString() + " seconds"
-    //       });
-    // })
+    it("search chicken and check load time when user is logged in", ()=>{
+        LoginPage.open();
+        LoginPage.login('jasmin.husadzic@gmail.com', 'test123');
+        LoginPage.waitForCart();
+        expect(HomePage.cart).toBeDisplayed;
+        let duration = HomePage.getSearchDuration('chicken');
+        expect(duration).toBeLessThanOrEqual(60);
+        TimelineReporter.addContext({
+            title: 'Search duration is',
+            value: duration.toString() + " seconds"
+          });
+    })
 
-    // it("search rice after we search for chicken and check load time when user is logged in", ()=>{
-    //     LoginPage.open();
-    //     LoginPage.login('jasmin.husadzic@gmail.com', 'test123');
-    //     LoginPage.waitForCart();  
-    //     expect(HomePage.cart).toBeDisplayed;
-    //     HomePage.search('chicken');
-    //     let duration = HomePage.getSearchDuration('rice');
-    //     expect(duration).toBeLessThanOrEqual(60);
-    //     TimelineReporter.addContext({
-    //         title: 'Search duration is',
-    //         value: duration.toString() + " seconds"
-    //       });
-    // })
+    it("search rice after we search for chicken and check load time when user is logged in", ()=>{
+        LoginPage.open();
+        LoginPage.login('jasmin.husadzic@gmail.com', 'test123');
+        LoginPage.waitForCart();  
+        expect(HomePage.cart).toBeDisplayed;
+        HomePage.search('chicken');
+        let duration = HomePage.getSearchDuration('rice');
+        expect(duration).toBeLessThanOrEqual(60);
+        TimelineReporter.addContext({
+            title: 'Search duration is',
+            value: duration.toString() + " seconds"
+          });
+    })
 
-    // it("search chicken, search rice and search chicken again and check load time for last search when user is logged in", ()=>{
-    //     LoginPage.open();
-    //     LoginPage.login('jasmin.husadzic@gmail.com', 'test123');
-    //     LoginPage.waitForCart();
-    //     expect(HomePage.cart).toBeDisplayed;
-    //     HomePage.search('chicken');
-    //     HomePage.search('rice');
-    //     let duration = HomePage.getSearchDuration('chicken');
-    //     expect(duration).toBeLessThanOrEqual(60);
-    //     TimelineReporter.addContext({
-    //         title: 'Search duration is',
-    //         value: duration.toString() + " seconds"
-    //       });
-    // })
+    it("search chicken, search rice and search chicken again and check load time for last search when user is logged in", ()=>{
+        LoginPage.open();
+        LoginPage.login('jasmin.husadzic@gmail.com', 'test123');
+        LoginPage.waitForCart();
+        expect(HomePage.cart).toBeDisplayed;
+        HomePage.search('chicken');
+        HomePage.search('rice');
+        let duration = HomePage.getSearchDuration('chicken');
+        expect(duration).toBeLessThanOrEqual(60);
+        TimelineReporter.addContext({
+            title: 'Search duration is',
+            value: duration.toString() + " seconds"
+          });
+    })
 
-    // it("search beef and check load time when user is logged in", ()=>{
-    //     LoginPage.open();
-    //     LoginPage.login('jasmin.husadzic@gmail.com', 'test123');  
-    //     expect(HomePage.cart).toBeDisplayed;
-    //     let duration = HomePage.getSearchDuration('beef');
-    //     expect(duration).toBeLessThanOrEqual(60);
-    //     TimelineReporter.addContext({
-    //         title: 'Search duration is',
-    //         value: duration.toString() + " seconds"
-    //       });
-    // })
+    it("search beef and check load time when user is logged in", ()=>{
+        LoginPage.open();
+        LoginPage.login('jasmin.husadzic@gmail.com', 'test123');  
+        expect(HomePage.cart).toBeDisplayed;
+        let duration = HomePage.getSearchDuration('beef');
+        expect(duration).toBeLessThanOrEqual(60);
+        TimelineReporter.addContext({
+            title: 'Search duration is',
+            value: duration.toString() + " seconds"
+          });
+    })
 
     afterEach(()=>{
         if(HomePage.cart.isDisplayed()){
