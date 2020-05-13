017cb47de94cfe8ef029b3a0988ea557a3da0dda
jasminhusadzic <jasmin.h@aleacontrol.com>
2020-05-13 21:26:39 +0200

    Add test case for add to order when user is logged

--- test/order.spec.js
+++ test/order.spec.js
@@ -3,25 +3,62 @@ import SearchData from '../data/search.data';
 import LoginData from '../data/login.data';
 import RestaurantPage from "../page_objects/pages/RestaurantPage";
 import LoginPage from "../page_objects/pages/LoginPage";
+import ProfilePage from "../page_objects/pages/ProfilePage";
 
-describe("order item with pickup", ()=>{
-
+describe("orders", ()=>{
+    
     beforeEach(()=>{
         HomePage.open();
         HomePage.prepareHome();
     });
 
-    it("add order from Marcello's and select pickup",()=>{
-        HomePage.search(SearchData.marchello, SearchData.marchelloLocation);
-        HomePage.clickOnRestaurantTab();
-        HomePage.clickOnViewRestaurant();
-        RestaurantPage.confirmInstructions();
-        RestaurantPage.order();
-        RestaurantPage.modalComponent.addToOrder();
-        LoginPage.login(LoginData.email, LoginData.password);
-        LoginPage.waitForCart();
-        RestaurantPage.modalComponent.selectPickup();
-        browser.pause(5000);
-    });
+   
+    describe("order item with pickup when user not logged", ()=>{
+
+
+        it("add order from Marcello's and select pickup",()=>{
+            HomePage.search(SearchData.marchello, SearchData.marchelloLocation);
+            HomePage.clickOnRestaurantTab();
+            HomePage.clickOnViewRestaurant();
+            RestaurantPage.confirmInstructions();
+            RestaurantPage.order();
+            RestaurantPage.modalComponent.addToOrder();
+            LoginPage.login(LoginData.email, LoginData.password);
+            LoginPage.waitForCart();
+            //RestaurantPage.modalComponent.selectPickup();
+            browser.pause(5000);
+        });
+
+    })
+
+    describe("order item with pickup when user is logged", ()=>{
+
+        beforeEach(()=>{
+            LoginPage.open();
+            LoginPage.login(LoginData.email, LoginData.password);
+            LoginPage.waitForCart();
+            expect(HomePage.cart).toBeDisplayed;
+        });
+
+        afterEach(()=>{
+            if(HomePage.cart.isDisplayed()){
+                ProfilePage.logout();
+            }
+            browser.deleteAllCookies();
+            browser.clearSessionStorage();
+        });
+        
+        fit("add order from Marcello's and select pickup with tip later",()=>{
+            HomePage.search(SearchData.marchello, SearchData.marchelloLocation);
+            HomePage.clickOnRestaurantTab();
+            HomePage.clickOnViewRestaurant();
+            RestaurantPage.confirmInstructions();
+            RestaurantPage.order();
+            RestaurantPage.modalComponent.addToOrder(); 
+            RestaurantPage.waitForOrderComplete();
+            browser.pause(5000);
+        });
+
+    })
 
 })
